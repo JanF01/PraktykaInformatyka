759538262efcd3e31445cec30a4a74c1b6687db0
JanF01 <jan.fiolka44@gmail.com>
2019-12-16 12:39:12 +0100

    repairing gallery

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(document).ready(function(){
 
   $('.sim-thumb').on('click', function() {
     $('#main-product-image').attr('src', $(this).data('image'));
-  })
+  });
 
   $('.sim-thumb2').on('click', function() {
     $('#main-product-image2').attr('src', $(this).data('image'));
-  })
+  });
 
   $('.mobilek').mouseover(function(){
        $('.mobileul').css('display','flex');
